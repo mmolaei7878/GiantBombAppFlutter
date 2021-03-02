ba853cb82560fc6b7094163ce7b7ed9117b45b2f
mohammad <mohammadmolaei7800@gmail.com>
2021-03-02 16:25:56 +0330

    loading ui .....

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -12,39 +12,151 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    super.initState();
     gameBloc.getGames();
   }
 
+  bool dashagh = false;
   @override
   Widget build(BuildContext context) {
+    final mqw = MediaQuery.of(context).size.width;
+    final mqh = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: StreamBuilder(
-        stream: gameBloc.subject,
-        builder: (ctx, AsyncSnapshot<GameResponse> snapShot) {
-          if (!snapShot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return SingleChildScrollView(
-              child: Column(
-                children: snapShot.data.gameList
-                    .map(
-                      (e) => ListTile(
-                        leading: Image.network(
-                          e.image.screen_large_url,
-                          width: 150,
-                          height: 150,
-                          fit: BoxFit.cover,
-                        ),
-                        title: Text(e.name),
+      backgroundColor: Color(0xff131418),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: mqh / 2,
+            elevation: 50,
+            backgroundColor: Color(0xff131418),
+            flexibleSpace: FlexibleSpaceBar(
+              background: Column(
+                children: [
+                  SizedBox(
+                    height: 150,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: EdgeInsets.only(left: 30),
+                    child: Text(
+                      'Browse the Games',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 55,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 32),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Color(0xff8764B8),
                       ),
-                    )
-                    .toList(),
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          suffixIcon: Container(
+                            margin: EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xff8764B8),
+                            ),
+                            child: Icon(
+                              Icons.navigate_next_sharp,
+                              color: Colors.white,
+                              size: 30,
+                            ),
+                          ),
+                          border: InputBorder.none,
+                          icon: Container(
+                            margin: EdgeInsets.only(left: 15),
+                            child: Icon(
+                              Icons.search,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.only(left: 5),
+                          labelStyle: TextStyle(color: Colors.white),
+                          labelText: 'search for games'),
+                    ),
+                  )
+                ],
               ),
-            );
-          }
-        },
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              <Widget>[
+                Container(
+                  width: mqw,
+                  height: mqh,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned(
+                        bottom: 0,
+                        width: mqw,
+                        height: mqh,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(50),
+                              topRight: Radius.circular(50),
+                            ),
+                            color: Color(0xff8764B8),
+                          ),
+                          child: StreamBuilder(
+                            stream: gameBloc.subject,
+                            builder:
+                                (ctx, AsyncSnapshot<GameResponse> snapShot) {
+                              if (!snapShot.hasData) {
+                                return Container(
+                                  alignment: Alignment.topCenter,
+                                  margin: EdgeInsets.only(top: 20),
+                                  width: mqw,
+                                  height: 70,
+                                  child: CircularProgressIndicator(
+                                    backgroundColor: Colors.black,
+                                    valueColor: AlwaysStoppedAnimation<Color>(
+                                        Colors.white),
+                                  ),
+                                );
+                              } else {
+                                return DraggableScrollableSheet(
+                                  expand: true,
+                                  initialChildSize: 0.9,
+                                  minChildSize: 0.6,
+                                  builder: (context, scrollController) =>
+                                      ListView.separated(
+                                    controller: scrollController,
+                                    separatorBuilder: (ctx, index) => SizedBox(
+                                      width: 10,
+                                      height: 10,
+                                    ),
+                                    itemCount: snapShot.data.gameList.length,
+                                    itemBuilder: (ctx, index) => ListTile(
+                                      title: Text(
+                                          snapShot.data.gameList[index].name),
+                                    ),
+                                  ),
+                                );
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
